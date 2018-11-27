aa3186c81089ec00913bcbe371e6c32027caeb39
MrceL <9719marcel@gmail.com>
2018-11-27 03:24:30 +0100

    Added some style to the homepage.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,16 @@
+// Modules
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
-
 import { AppRoutingModule } from './app-routing.module';
+import { NavbarModule, WavesModule, ButtonsModule } from 'angular-bootstrap-md'
+
+// Website-Components
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
+
+
 
 @NgModule({
   declarations: [
@@ -16,7 +21,10 @@ import { NavbarComponent } from './navbar/navbar.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MDBBootstrapModule.forRoot()
+    MDBBootstrapModule.forRoot(),
+    NavbarModule,
+    WavesModule,
+    ButtonsModule
   ],
   schemas: [ NO_ERRORS_SCHEMA ],
   providers: [],
